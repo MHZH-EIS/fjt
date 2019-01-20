1cb22c8a000bfdaafb221a958285758b19ccda31
Cyony <31874236+Cyony@users.noreply.github.com>
2019-01-20 13:22:15 +0800

    标准管理

--- src/main/java/com/ai/eis/common/Tools.java
+++ src/main/java/com/ai/eis/common/Tools.java
@@ -1,9 +1,14 @@
 package com.ai.eis.common;
 
+import org.apache.commons.lang.StringUtils;
+
 public class Tools {
 
     public static String liker(String field) {
-        return "%" + field + "%";
+        if (!StringUtils.isEmpty(field)) {
+            field = "%" + field + "%";
+        }
+        return field;
     }
 
 }
